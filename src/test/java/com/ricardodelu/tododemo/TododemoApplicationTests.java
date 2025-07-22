ec4979e1ecd184f5feff49e79138a36a3998732d
ricardodelu <riluso@gmail.com>
2025-07-21 19:06:43 -0600

    initialize Spring Boot project with basic structure and configuration

---
+++ src/test/java/com/ricardodelu/tododemo/TododemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardodelu.tododemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TododemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
